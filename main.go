0cbbab26866fec414698bdec365525d81023d04b
Wissame MEKHILEF <wissame.mekhilef@gmail.com>
2022-04-18 16:48:43 +0000

    Solution to problem 3 with Sieve of Eratosthenes

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	// "github.com/WissameMekhilef/euler-project/ex_1"
-	"github.com/WissameMekhilef/euler-project/ex_2"
+	// "github.com/WissameMekhilef/euler-project/ex_2"
+	"github.com/WissameMekhilef/euler-project/ex_3"
 )
 
 func main() {
 	// ex_1.Run()
-	ex_2.Run()
+	// ex_2.Run()
+	ex_3.Run()
 }
